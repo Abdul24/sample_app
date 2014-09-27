ab35be25227b1c18278270a64fe247fc0dfa01ff
abdulaleem seyed <aseyed24@gmail.com>
2014-09-27 17:40:38 -0400

    Add account activation, password resets & email configuration

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -48,9 +48,17 @@ module SessionsHelper
     @current_user = nil
   end
 
-  # Returns the current logged-in user (if any).
+  #Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   # Redirects to stored location (or to the default).
@@ -63,6 +71,6 @@ module SessionsHelper
   def store_location
     session[:forwarding_url] = request.url if request.get?
   end
-end
 
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
 
-  root                 'static_pages#home'
+  get 'password_resets/new'
 
-  get    'help'     => 'static_pages#help'
+  get 'password_resets/edit'
 
-  get    'about'    => 'static_pages#about'
-
-  get    'contact'  => 'static_pages#contact'
-
-  get    'signup'   => 'users#new'
-
-  get    'login'    => 'sessions#new'
-
-  delete 'logout'   => 'sessions#destroy'
-  
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
-
   resources :sessions, only: [:new, :create, :destroy]
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
